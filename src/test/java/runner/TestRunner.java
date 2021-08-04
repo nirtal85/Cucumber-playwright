c605cc3ce45ba8e9bcb38e2e60486cceab9d51a9
Nir Tal <nirt236@gmail.com>
2021-08-04 08:26:11 +0300

    allure results

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,6 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "classpath:features",
+        plugin = "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm",
         tags = "not @Ignore",
         glue = "steps"
 )
